0e11c796bde4b747ef7ef0934ade763d04ac9f7f
Anna Clara <annaclaraaraujo229@gmail.com>
2025-05-13 16:54:45 -0300

    feat: interligando Api ao React

--- Event_React/Event/src/components/cadastro/Cadastro.jsx
+++ Event_React/Event/src/components/cadastro/Cadastro.jsx
@@ -1,31 +1,35 @@
 import "./Cadastro.css";
 import Botao from "../botao/Botao";
-const Cadastro = (props) => {
+const Cadastro = (promps) => {
   return (
     <section className="section_cadastro">
-      <div className="Titulo_cadastro">
-        <h1>{props.tituloCadastro}</h1>
-        <hr />
-      </div>
-
       <form action="" className="layout_grid form_cadastro">
-        <img className="img_Cadastro" src={props.img_banner} alt="" />
+          <h1>{promps.tituloCadastro}</h1>
+          <hr />
+
         <div className="campos_cadastro">
-          <div className="campo_cad_nome">
-            <input type="text" placeholder={props.campoPlaceholder} />
+          <div className="banner_cadastro">
+            <img src={promps.img_banner} alt="" />
           </div>
 
-          <div className="campo_cad_tipoEvento" style={{display:props.visibilidade}}>
-                            <label htmlFor="tipoEvento"></label>
-                            <select name="tipoEvento" id="">
-                                <option value="" disabled selected>Tipo Evento</option>
-                                <option value="">Festa</option>
-                                <option value="">Social</option>
-                                <option value="">Trabalho</option>
-                            </select>
-                        </div>
+          <div className="campo_preen">
 
-          <Botao nomeBotao= "Cadastrar" />
+            <div className="campo_cad_nome">
+              <label></label>
+              <input type="text" placeholder={promps.campoPlaceholder} />
+            </div>
+
+            <div className="campo_cad_tipoEvento" style={{ display: promps.visibilidade }}>
+              <label htmlFor="tipoEvento"></label>
+              <select name="tipoEvento" id="">
+                <option value="" disabled selected>Tipo Evento</option>
+                <option value="">Festa</option>
+                <option value="">Social</option>
+                <option value="">Trabalho</option>
+              </select>
+            </div>
+            <Botao nomeBotao="Cadastrar" />
+          </div>
         </div>
       </form>
     </section>
